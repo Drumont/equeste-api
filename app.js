44b0f2526f56870ca9653a746b5ee641e534fb74
Drumont Erick <drumont06@gmail.com>
2020-09-18 01:17:31 +0200

    Revert "Auth and Jwt done"
    This reverts commit c1fdf47d01aad037bea1b4fef1b674fb8444338d.

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 const express = require('express')
 const app = express();
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const apiRouter = require('./apiRouter').router;
-const response = require('./apiRouter');
 
 // Morgan
 app.use(morgan('dev'));
-
-// Body Parser Configuration
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
-
 
 // Middleware
 app.use(((req, res, next) => {
@@ -19,12 +11,9 @@ app.use(((req, res, next) => {
 }))
 
 app.get('/api/v1', (req, res) => {
-    res.json(response.success('API version 1'));
+    res.json(success('API version 1'));
 })
 
-app.use('/api/', apiRouter);
-
-//Launch server
 app.listen(8080, ()=> {
     console.log('Started on port 8080');
 })
